5a58a4b06b858ccecc9a1189c2dec987808795d7
arne <arne@decsai.ugr.es>
2023-01-12 15:29:42 +0100

    small fix

--- src/utils/segmentation_backbone.py
+++ src/utils/segmentation_backbone.py
@@ -24,14 +24,6 @@ def create_segmentation_backbone():
             classes=class_no  # model output channels (number of classes in your dataset)
         )
     elif config['model']['backbone'] == 'deeplabv3p':
-        seg_model = UnetPlusPlus(
-            encoder_name=config['model']['encoder']['backbone'],  # choose encoder, e.g. mobilenet_v2 or efficientnet-b7
-            encoder_weights=config['model']['encoder']['weights'],
-            # use `imagenet` pre-trained weights for encoder initialization
-            in_channels=3,  # model input channels (1 for gray-scale images, 3 for RGB, etc.)
-            classes=class_no  # model output channels (number of classes in your dataset)
-        )
-    elif config['model']['backbone'] == 'deeplabv3p':
         seg_model = DeepLabV3Plus(
             encoder_name=config['model']['encoder']['backbone'],  # choose encoder, e.g. mobilenet_v2 or efficientnet-b7
             encoder_weights=config['model']['encoder']['weights'],
@@ -41,6 +33,14 @@ def create_segmentation_backbone():
         )
     elif config['model']['backbone'] == 'pspnet':
         seg_model = PSPNet(
+            encoder_name=config['model']['encoder']['backbone'],  # choose encoder, e.g. mobilenet_v2 or efficientnet-b7
+            encoder_weights=config['model']['encoder']['weights'],
+            # use `imagenet` pre-trained weights for encoder initialization
+            in_channels=3,  # model input channels (1 for gray-scale images, 3 for RGB, etc.)
+            classes=class_no  # model output channels (number of classes in your dataset)
+        )
+    elif config['model']['backbone'] == 'linknet':
+        seg_model = Linknet(
             encoder_name=config['model']['encoder']['backbone'],  # choose encoder, e.g. mobilenet_v2 or efficientnet-b7
             encoder_weights=config['model']['encoder']['weights'],
             # use `imagenet` pre-trained weights for encoder initialization
