fix softmax in loss issue

src/Probabilistic_Unet_Pytorch/probabilistic_unet.py
@@ -150,6 +150,7 @@ class Fcomb(nn.Module):
             else:
                 self.layers.apply(init_weights)
                 self.last_layer.apply(init_weights)
+        self.activation = torch.nn.Softmax(dim=1)
 
     def tile(self, a, dim, n_tile):
         """
@@ -163,7 +164,7 @@ class Fcomb(nn.Module):
         order_index = torch.LongTensor(np.concatenate([init_dim * np.arange(n_tile) + i for i in range(init_dim)])).to(device)
         return torch.index_select(a, dim, order_index)
 
-    def forward(self, feature_map, z):
+    def forward(self, feature_map, z, use_softmax=True):
         """
         Z is batch_sizexlatent_dim and feature_map is batch_sizexno_channelsxHxW.
         So broadcast Z to batch_sizexlatent_dimxHxW. Behavior is exactly the same as tf.tile (verified)
@@ -177,8 +178,10 @@ class Fcomb(nn.Module):
             #Concatenate the feature map (output of the UNet) and the sample taken from the latent space
             feature_map = torch.cat((feature_map, z), dim=self.channel_axis)
             output = self.layers(feature_map)
-            return self.last_layer(output)
-
+            output = self.last_layer(output)
+            if use_softmax:
+                output = self.activation(output)
+            return output
 
 
 class ProbabilisticUnet(nn.Module):
@@ -225,7 +228,7 @@ class ProbabilisticUnet(nn.Module):
         else:
             self.unet_features = self.unet.forward(patch)
 
-    def sample(self, testing=False):
+    def sample(self, testing=False, use_softmax=True):
         """
         Sample a segmentation by reconstructing from a prior sample
         and combining this with UNet features
@@ -238,8 +241,17 @@ class ProbabilisticUnet(nn.Module):
             z_prior = self.prior_latent_space.base_dist.loc
             # z_prior = self.prior_latent_space.sample()
             self.z_prior_sample = z_prior
-        return self.fcomb.forward(self.unet_features, z_prior)
+        return self.fcomb.forward(self.unet_features, z_prior, use_softmax)
 
+    def get_gold_predictions(self):
+        shape = [self.mc_samples, self.unet_features.shape[0], self.num_classes,
+                 self.unet_features.shape[-2], self.unet_features.shape[-1]]
+        samples = torch.zeros(shape).to(device)
+        for i in range(10):
+            samples[i] = self.sample(use_softmax=True)
+        mean = torch.mean(samples, dim=0)
+
+        return mean
 
     def reconstruct(self, use_posterior_mean=False, calculate_posterior=False, z_posterior=None):
         """
@@ -252,7 +264,7 @@ class ProbabilisticUnet(nn.Module):
         else:
             if calculate_posterior:
                 z_posterior = self.posterior_latent_space.rsample()
-        return self.fcomb.forward(self.unet_features, z_posterior)
+        return self.fcomb.forward(self.unet_features, z_posterior, use_softmax=False)
 
     def kl_divergence(self, analytic=True, calculate_posterior=False, z_posterior=None):
         """
@@ -298,6 +310,7 @@ class ProbabilisticUnet(nn.Module):
         return loss
 
     def train_step(self, images, labels, loss_fct, ann_ids=None):
+        # for training don't use softmax because it is integrated in loss function
         self.forward(images, labels, training=True)
         loss = self.combined_loss(labels, loss_fct)
         y_pred = self.reconstruction

src/model_handler.py
@@ -103,14 +103,7 @@ class ModelHandler():
         class_no = config['data']['class_no']
         vis_images = config['data']['visualize_images'][mode]
 
-        if mode=='test':
-            print("Testing the best model")
-            model_dir = 'models'
-            dir = os.path.join(globals.config['logging']['experiment_folder'], model_dir)
-            model_path = os.path.join(dir, 'best_model.pth')
-            model = torch.load(model_path)
-        else:
-            model = self.model
+        model = self.model
 
         device = self.device
         model.eval()
@@ -133,7 +126,7 @@ class ModelHandler():
                             test_pred = model.sample(use_z_mean=True, annotator_ids=ann_id, annotator_list=annotator_list)
                     elif config['model']['method'] == 'prob_unet':
                         model.forward(test_img, None, training=False)
-                        test_pred = model.sample(testing=True)
+                        test_pred = model.get_gold_predictions()
                     else:
                         test_pred = model(test_img)
                     _, test_pred = torch.max(test_pred[:, 0:class_no], dim=1)

src/utils/initialize_optimization.py
@@ -37,16 +37,12 @@ def init_optimization(model):
 
     loss_mode = config['model']['loss']
     if loss_mode == 'ce':
-        nlloss = torch.nn.NLLLoss(reduction='mean', ignore_index=ignore_index, weight=class_weights)
-        def ce_with_softmax(preds, labels):
-            return nlloss(torch.log(preds+1e-7), labels)
-
-        loss_fct = ce_with_softmax
+        loss_fct = torch.nn.CrossEntropyLoss(weight=class_weights, ignore_index=ignore_index)
     elif loss_mode == 'dice':
         # loss_fct = DiceLoss(ignore_index=ignore_index, mode='multiclass', from_logits=False)
-        loss_fct = DiceLoss(weight=class_weights, normalization='none')
+        loss_fct = DiceLoss(weight=class_weights, normalization='softmax')
     elif loss_mode == 'gdice':
-        loss_fct = GeneralizedDiceLoss(normalization='none')
+        loss_fct = GeneralizedDiceLoss(normalization='softmax')
     elif loss_mode == 'focal':
         loss_fct = FocalLoss(reduction='mean', ignore_index=ignore_index, mode='multiclass')
     else:

src/utils/model_confusionmatrix.py
@@ -116,11 +116,13 @@ class ConfusionMatrixModel(torch.nn.Module):
         self.activation = torch.nn.Softmax(dim=1)
 
 
-    def forward(self, x):
+    def forward(self, x, use_softmax=True):
         y = self.seg_model(x)
+        if use_softmax:
+            y = self.activation(y)
         return y
 
-    def forward_with_cms(self, x):
+    def forward_with_cms(self, x, use_softmax=True):
         x = self.seg_model.encoder(x)
         x = self.seg_model.decoder(*x)
         cms = []
@@ -129,8 +131,9 @@ class ConfusionMatrixModel(torch.nn.Module):
             #cm_ = cm[0, :, :, 0, 0]
             #print("CM! ", cm_ / cm_.sum(0, keepdim=True))
             cms.append(cm)
-        x = self.seg_model.segmentation_head(x)
-        y = self.activation(x)
+        y = self.seg_model.segmentation_head(x)
+        if use_softmax:
+            y = self.activation(y)
         return y, cms
 
     def get_used_cms(self, cms, ann_ids):
@@ -165,7 +168,8 @@ class ConfusionMatrixModel(torch.nn.Module):
         return pred_noisy, cm
 
     def train_step(self, images, labels, loss_fct, ann_ids):
-        y_pred, cms = self.forward_with_cms(images)
+        # for training don't use softmax because it is integrated in loss function
+        y_pred, cms = self.forward_with_cms(images, use_softmax=False)
         cms_used = self.get_used_cms(cms, ann_ids)
         pred_noisy, cms_used = self.get_noisy_pred(y_pred, cms_used)
         # log_likelihood_loss = loss_fct(pred_noisy, labels.view(b, h, w).long())

src/utils/model_pionono.py
@@ -140,7 +140,7 @@ class PiononoHead(nn.Module):
             np.concatenate([init_dim * np.arange(n_tile) + i for i in range(init_dim)])).to(device)
         return torch.index_select(a, dim, order_index)
 
-    def forward(self, feature_map, z):
+    def forward(self, feature_map, z, use_softmax=True):
         """
         Z is batch_sizexlatent_dim and feature_map is batch_sizexno_channelsxHxW.
         So broadcast Z to batch_sizexlatent_dimxHxW. Behavior is exactly the same as tf.tile (verified)
@@ -154,8 +154,9 @@ class PiononoHead(nn.Module):
             # Concatenate the feature map (output of the UNet) and the sample taken from the latent space
             feature_map = torch.cat((feature_map, z), dim=self.channel_axis)
             x = self.layers(feature_map)
-            x = self.last_layer(x)
-            y = self.activation(x)
+            y = self.last_layer(x)
+            if use_softmax:
+                y = self.activation(y)
             return y
 
 class PiononoModel(nn.Module):
@@ -208,7 +209,7 @@ class PiononoModel(nn.Module):
             new_ids[a] = torch.nonzero(torch.tensor(annotator_list[int(annotator_ids[a])] == np.array(self.annotators)))[0][0]
         return new_ids
 
-    def sample(self, use_z_mean: bool, annotator_ids: torch.tensor, annotator_list: list = None):
+    def sample(self, use_z_mean: bool, annotator_ids: torch.tensor, annotator_list: list = None, use_softmax=True):
         """
         Sample a segmentation by reconstructing from a prior sample
         and combining this with UNet features
@@ -220,31 +221,33 @@ class PiononoModel(nn.Module):
             z = self.z.forward(annotator_ids, sample=True)
         else:
             z = self.z.forward(annotator_ids, sample=False)
-        pred = self.head.forward(self.unet_features, z)
+        pred = self.head.forward(self.unet_features, z, use_softmax)
 
         return pred
 
     def get_gold_predictions(self):
         if len(self.gold_annotators) == 1:
             annotator = torch.ones(self.unet_features.shape[0]).to(device) * self.gold_annotators[0]
-            mean, std = self.mc_sampling(annotator)
+            mean, std = self.mc_sampling(annotator, use_softmax=True)
         else:
             shape = [self.mc_samples * len(self.gold_annotators), self.unet_features.shape[0], self.num_classes,
                      self.unet_features.shape[-2], self.unet_features.shape[-1]]
             samples = torch.zeros(shape).to(device)
             for a in range(len(self.gold_annotators)):
                 for i in range(self.mc_samples):
+                    annotator_ids = torch.ones(self.unet_features.shape[0]).to(device) * self.gold_annotators[a]
                     samples[(a*self.mc_samples)+i] = self.sample(use_z_mean=False,
-                                                                 annotator_ids=torch.ones(self.unet_features.shape[0]).to(device) * self.gold_annotators[a])
+                                                                 annotator_ids=annotator_ids,
+                                                                 use_softmax=True)
             mean = torch.mean(samples, dim=0)
             std = torch.std(samples, dim=0)
         return mean, std
 
-    def mc_sampling(self, annotator: torch.tensor = None):
+    def mc_sampling(self, annotator: torch.tensor = None, use_softmax=True):
         shape = [self.mc_samples, annotator.shape[0], self.num_classes, self.unet_features.shape[-2], self.unet_features.shape[-1]]
         samples = torch.zeros(shape).to(device)
         for i in range(self.mc_samples):
-            samples[i] = self.sample(use_z_mean=False, annotator_ids=annotator)
+            samples[i] = self.sample(use_z_mean=False, annotator_ids=annotator, use_softmax=use_softmax)
         mean = torch.mean(samples, dim=0)
         std = torch.std(samples, dim=0)
         return mean, std
@@ -254,7 +257,7 @@ class PiononoModel(nn.Module):
         Calculate the evidence lower bound of the log-likelihood of P(Y|X)
         """
         # self.preds = self.sample(use_z_mean=False, annotator=annotator)
-        self.preds, _ = self.mc_sampling(annotator=annotator)
+        self.preds, _ = self.mc_sampling(annotator=annotator, use_softmax=False)
         self.log_likelihood_loss = loss_fct(self.preds, labels)
         self.kl_loss = self.z.get_kl_loss(annotator) * self.kl_factor
 

src/utils/model_supervised.py
@@ -7,7 +7,7 @@ class SupervisedSegmentationModel(torch.nn.Module):
         self.seg_model = create_segmentation_backbone()
         self.activation = torch.nn.Softmax(dim=1)
 
-    def forward(self, x):
+    def forward(self, x, use_softmax=False):
         x = self.seg_model(x)
         y = self.activation(x)
         return y